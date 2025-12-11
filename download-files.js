470b9896702539a2197a8c1a4ca8d3c5103a20c5
Benja C. <benjamincervigni@gmail.com>
2025-12-11 13:00:37 -0300

    fix add livekitagents

--- download-files.js
+++ download-files.js
@@ -1,6 +1,6 @@
 // download-files.js
 
-import { downloadFiles } from "@livekit/agents-plugin-livekit";
+import { downloadFiles } from "@livekit/agents";
 
 // Descarga los modelos necesarios ANTES de iniciar el bot
 console.log("⬇️ Descargando modelos de LiveKit...");
